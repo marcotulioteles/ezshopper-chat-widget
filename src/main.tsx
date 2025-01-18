f7d7679bfed0fc28ec6644f7c01d6dd6ff438ee5
Marco Túlio Teles dos Reis <marcotuliod3v@gmail.com>
2025-01-18 17:30:31 -0300

    :sparkles: feat: set initialization methods in the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,48 @@
 // ==== For local tests ========================================================
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
-// ==== For production =========================================================
-
 // import { StrictMode } from "react";
 // import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "./App.tsx";
-// import { initializeCSSVariables } from "./utils/initialize-css-variables.ts";
 
-// /**
-//  * Initializes the chatbot and attaches it to a specified DOM element.
-//  * @param config - Configuration object with target selector.
-//  * @example
-//  * createChat({ target: '#chat-content' });
-//  */
-// export const createChat = (config: {
-//   target: string;
-//   primaryColor?: string;
-// }) => {
-//   // Initialize global CSS variable
-//   const primaryColor = config.primaryColor || "#000000"; // Default color
-//   initializeCSSVariables(primaryColor);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
 
-//   const targetElement = document.querySelector(config.target);
+// ==== For production =========================================================
 
-//   if (!targetElement) {
-//     console.error(`Target element ${config.target} not found.`);
-//     return;
-//   }
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { initializeCSSVariables } from "./utils/initialize-css-variables.ts";
 
-//   const root = createRoot(targetElement);
-//   root.render(
-//     <StrictMode>
-//       <App />
-//     </StrictMode>
-//   );
-// };
+/**
+ * Initializes the chatbot and attaches it to a specified DOM element.
+ * @param config - Configuration object with target selector.
+ * @example
+ * createChat({ target: '#chat-content' });
+ */
+export const createChat = (config: {
+  target: string;
+  primaryColor?: string;
+}) => {
+  // Initialize global CSS variable
+  const primaryColor = config.primaryColor || "#000000"; // Default color
+  initializeCSSVariables(primaryColor);
+
+  const targetElement = document.querySelector(config.target);
+
+  if (!targetElement) {
+    console.error(`Target element ${config.target} not found.`);
+    return;
+  }
+
+  const root = createRoot(targetElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
