5a0ca5f6ef29ac0c0ec854e4b42527cbaa457695
Marco Túlio Teles dos Reis <marcotuliod3v@gmail.com>
2025-01-22 15:18:09 -0300

    :sparkles: feat: set main.tsx to production configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,63 +1,63 @@
 // ==== For local tests ========================================================
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { ChatConfigProvider } from "./contexts/chat-config.context.tsx";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ChatConfigProvider
-      value={{
-        shopId: "",
-        webhookUrl:
-          "https://ezshopper-shopify-webapi.azurewebsites.net/api/chatbot-realtime/SendMessage",
-      }}
-    >
-      <App />
-    </ChatConfigProvider>
-  </StrictMode>
-);
-
-// ==== For production =========================================================
-
 // import { StrictMode } from "react";
 // import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "./App.tsx";
-// import { initializeCSSVariables } from "./utils/initialize-css-variables.ts";
 // import { ChatConfigProvider } from "./contexts/chat-config.context.tsx";
 
-// /**
-//  * Initializes the chatbot and attaches it to a specified DOM element.
-//  * @param config - Configuration object with target selector.
-//  * @example
-//  * createChat({ target: '#chat-content' });
-//  */
-// export const createChat = (config: {
-//   shopId: string;
-//   webhookUrl: string;
-//   target: string;
-//   customerId?: string;
-//   primaryColor?: string;
-// }) => {
-//   // Initialize global CSS variable
-//   const primaryColor = config.primaryColor || "#000000"; // Default color
-//   initializeCSSVariables(primaryColor);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <ChatConfigProvider
+//       value={{
+//         shopId: "",
+//         webhookUrl:
+//           "https://ezshopper-shopify-webapi.azurewebsites.net/api/chatbot-realtime/SendMessage",
+//       }}
+//     >
+//       <App />
+//     </ChatConfigProvider>
+//   </StrictMode>
+// );
 
-//   const targetElement = document.querySelector(config.target);
+// ==== For production =========================================================
 
-//   if (!targetElement) {
-//     console.error(`Target element ${config.target} not found.`);
-//     return;
-//   }
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { initializeCSSVariables } from "./utils/initialize-css-variables.ts";
+import { ChatConfigProvider } from "./contexts/chat-config.context.tsx";
 
-//   const root = createRoot(targetElement);
-//   root.render(
-//     <StrictMode>
-//       <ChatConfigProvider value={config}>
-//         <App />
-//       </ChatConfigProvider>
-//     </StrictMode>
-//   );
-// };
+/**
+ * Initializes the chatbot and attaches it to a specified DOM element.
+ * @param config - Configuration object with target selector.
+ * @example
+ * createChat({ target: '#chat-content' });
+ */
+export const createChat = (config: {
+  shopId: string;
+  webhookUrl: string;
+  target: string;
+  customerId?: string;
+  primaryColor?: string;
+}) => {
+  // Initialize global CSS variable
+  const primaryColor = config.primaryColor || "#000000"; // Default color
+  initializeCSSVariables(primaryColor);
+
+  const targetElement = document.querySelector(config.target);
+
+  if (!targetElement) {
+    console.error(`Target element ${config.target} not found.`);
+    return;
+  }
+
+  const root = createRoot(targetElement);
+  root.render(
+    <StrictMode>
+      <ChatConfigProvider value={config}>
+        <App />
+      </ChatConfigProvider>
+    </StrictMode>
+  );
+};
